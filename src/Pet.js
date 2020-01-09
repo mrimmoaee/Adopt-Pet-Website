88f75043d9ce7940241f83fdb38200981db26cb9
Shamee1015 <mrimmoaee@gmail.com>
2020-01-09 13:07:42 +0600

    finish hook

--- src/Pet.js
+++ src/Pet.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 export default function Pet({ name, animal, breed }) {
-  //destructuring by removing props
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h3", {}, breed)
-  ]);
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
 }
